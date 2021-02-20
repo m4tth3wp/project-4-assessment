7802d33acb8dca596868ea50a56025c969932620
Matt <matt-paul@live.com>
2021-02-20 15:00:53 -0500

    completed clicking the buttons

--- src/App.js
+++ src/App.js
@@ -7,24 +7,43 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      ...this.getInitialState(),
       circle1: '',
       circle2: '',
       circle3: '',
-      circle4: ''
+      circle4: '',
     };
   }
 
-  getInitialState() {
-    return {
-    };
+  handleClick1 = () => {
+    this.setState({circle1: 'yes'})
   }
+
+  handleClick2 = () => {
+    this.setState({circle2: 'yes'})
+  }
+
+  handleClick3 = () => {
+    this.setState({circle3: 'yes'})
+  }
+
+  handleClick4 = () => {
+    this.setState({circle4: 'yes'})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
         <main>
-          <CircleSelector/>
+          <CircleSelector 
+            handle={this.handleClick1}
+            handle2={this.handleClick2}
+            handle3={this.handleClick3}
+            handle4={this.handleClick4}            
+            selected={this.state.circle1}
+            selected2={this.state.circle2}
+            selected3={this.state.circle3}
+            selected4={this.state.circle4}/>
           <Circles/>
         </main>
       </div>
--- src/components/CircleSelector/CircleSelector.jsx
+++ src/components/CircleSelector/CircleSelector.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './CircleSelector.css'
 
-const CircleSelector = (props) => (
+class CircleSelector extends React.Component {
+    
+    render() {
+        return(
     <div className='CircleSelector'>
-        <button>Circle 1 Selected</button>
-        <button>Circle 2 Selected</button>
-        <button>Circle 3 Selected</button>
-        <button>Circle 4 Selected</button>
+        <button onClick={this.props.handle} className={ this.props.selected === 'yes' ? 'selected' : ''} >Circle 1 Selected</button>
+        <button onClick={this.props.handle2} className={ this.props.selected2 === 'yes' ? 'selected' : ''}  >Circle 2 Selected</button>
+        <button onClick={this.props.handle3} className={ this.props.selected3 === 'yes' ? 'selected' : ''} >Circle 3 Selected</button>
+        <button onClick={this.props.handle4} className={ this.props.selected4 === 'yes' ? 'selected' : ''} >Circle 4 Selected</button>
     </div>
-);
+        )}
+}
+
 
 export default CircleSelector;
